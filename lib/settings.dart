9b9c9b3e4831faac71fa46df27955f98052986cb
yeeet7 <viktor.kovalik171@gmail.com>
2023-07-23 15:36:27 +0200

    settings update and backup screen layout

--- lib/settings.dart
+++ lib/settings.dart
@@ -1,9 +1,11 @@
 
-
+import 'dart:math' as math;
 import 'package:cloudstream/widgets.dart';
 import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:hive_flutter/hive_flutter.dart';
+import 'package:package_info_plus/package_info_plus.dart';
 
 class Settings extends StatelessWidget {
   const Settings({super.key});
@@ -18,7 +20,13 @@ class Settings extends StatelessWidget {
         children: [
           Button(text: 'General', icon: const Icon(Icons.arrow_forward_ios_rounded), onTap: () {Navigator.push(context, MaterialPageRoute(builder: (context) => const GeneralSettings()));},),
           Button(text: 'Player', icon: const Icon(Icons.arrow_forward_ios_rounded), onTap: () {Navigator.push(context, MaterialPageRoute(builder: (context) => const PlayerSettings()));},),
-          const Text('0.3.0'),
+          Button(text: 'Updates and Backup', icon: const Icon(Icons.arrow_forward_ios_rounded), onTap: () {Navigator.push(context, MaterialPageRoute(builder: (context) => const BackupSettings()));},),
+          FutureBuilder(
+            future: PackageInfo.fromPlatform().then((value) => value.version),
+            builder: (context, snapshot) {
+              return Text('v${snapshot.data ?? '0.0.0'}');
+            }
+          ),
         ],
       )
 
@@ -27,11 +35,12 @@ class Settings extends StatelessWidget {
 }
 
 class SettingsButton extends StatelessWidget {
-  const SettingsButton({required this.text, required this.icon, this.subtitle, this.onTap, super.key});
+  const SettingsButton({required this.text, required this.icon, this.switchValue, this.subtitle, this.onTap, super.key});
   final String text;
-  final String? subtitle;
+  final Widget? subtitle;
   final void Function()? onTap;
   final Widget icon;
+  final bool? switchValue;
 
   @override
   Widget build(BuildContext context) {
@@ -42,17 +51,29 @@ class SettingsButton extends StatelessWidget {
         child: Padding(
           padding: const EdgeInsets.all(15),
           child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
-              icon,
-              const SizedBox(width: 25),
-              Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.start,
+              Row(
                 children: [
-                  Text(text, style: const TextStyle(fontSize: 16),),
-                  if(subtitle != null) Text('$subtitle', style: const TextStyle(fontSize: 12, color: Colors.white54),),
+                  icon,
+                  const SizedBox(width: 25),
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(text, style: const TextStyle(fontSize: 16),),
+                      if(subtitle != null) subtitle!,//style = TextStyle(fontSize: 12, color: Colors.white54),
+                    ],
+                  ),
                 ],
               ),
+              if(switchValue != null) Switch(
+                value: switchValue!,
+                activeColor: Theme.of(context).primaryColor,
+                onChanged: (value) async {
+                  onTap?.call();
+                }
+              )
             ],
           )
         ),
@@ -84,7 +105,7 @@ class _GeneralSettingsState extends State<GeneralSettings> {
             SettingsButton(
               text: 'Download path',
               icon: PictureIcon('assets/download.png'),
-              subtitle: '${Hive.box('config').get('downloadPath',) ?? '/storage/emulated/0/Download'}',
+              subtitle: Text('${Hive.box('config').get('downloadPath',) ?? '/storage/emulated/0/Download'}', style: const TextStyle(fontSize: 12, color: Colors.white54)),
               onTap: () async {
                 await setDownloadPath();
                 setState(() {});
@@ -113,6 +134,63 @@ class PlayerSettings extends StatelessWidget {
       body: const SingleChildScrollView(
         child: Column(
           children: [
+          ],
+        ),
+      ),
+
+    );
+  }
+}
+
+class BackupSettings extends StatefulWidget {
+  const BackupSettings({super.key});
+
+  @override
+  State<BackupSettings> createState() => _BackupSettingsState();
+}
+
+class _BackupSettingsState extends State<BackupSettings> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+      appBar: AppBar(
+        leading: IconButton(onPressed: () => Navigator.pop(context), icon: const Icon(Icons.arrow_back_ios_new_rounded)),
+        title: const Text('updates and Backup'),
+      ),
+
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(margin: const EdgeInsets.only(left: 64, top: 16), child: Text('Updates', style: TextStyle(color: Theme.of(context).primaryColor, fontWeight: FontWeight.bold),)),
+            SettingsButton(
+              text: 'Auto update',
+              subtitle: const Text('Automatically checks for updates', style: TextStyle(fontSize: 12, color: Colors.white54)),
+              icon: const Icon(Icons.phone_android_rounded),
+              switchValue: Hive.box('config').get('checkForUpdates') ?? true,
+              onTap: () async {
+                await Hive.box('config').put('checkForUpdates', !(Hive.box('config').get('checkForUpdates') ?? true));
+                setState(() {});
+              },
+            ),
+            SettingsButton(
+              text: 'Check for update',
+              subtitle: FutureBuilder(future: PackageInfo.fromPlatform().then((value) => value.version), builder: (context, snapshot) {return Text('v${snapshot.data ?? '0.0.0'}', style: const TextStyle(fontSize: 12, color: Colors.white54));}),
+              icon: const Icon(Icons.phone_android_rounded),
+              onTap: () {},//TODO
+            ),
+            Container(margin: const EdgeInsets.only(left: 64, top: 16), child: Text('Backup', style: TextStyle(color: Theme.of(context).primaryColor, fontWeight: FontWeight.bold),)),
+            SettingsButton(
+              text: 'Restore data from backup',
+              icon: Transform.rotate(angle: math.pi/2, child: const Icon(FontAwesomeIcons.arrowRightToBracket)),
+              onTap: () {},//TODO
+            ),
+            SettingsButton(
+              text: 'Back up data',
+              icon: Transform.rotate(angle: math.pi*1.5, child: const Icon(FontAwesomeIcons.arrowRightFromBracket)),
+              onTap: () {},//TODO
+            ),
           ],
         ),
       ),
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,10 +5,12 @@
 import FlutterMacOS
 import Foundation
 
+import package_info_plus
 import path_provider_foundation
 import screen_brightness_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FLTPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FLTPackageInfoPlusPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   ScreenBrightnessMacosPlugin.register(with: registry.registrar(forPlugin: "ScreenBrightnessMacosPlugin"))
 }
