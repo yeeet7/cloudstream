a2913d0e6bb41a5586d5f93c4f5c3a961ad0b4a4
yeeet7 <viktor.kovalik171@gmail.com>
2023-07-25 19:16:54 +0200

    oggg

--- lib/downloads.dart
+++ lib/downloads.dart
@@ -107,7 +107,7 @@ class _DownloadsState extends State<Downloads> {
                       Builder(
                         builder: (context) {
                           List sizelist = [];
-                          Directory(Hive.box('config').get('downloadPath') ?? '/storage/emulated/0/Download/').listSync().where((element) => RegExp('mp4|m4v|m4p|amv|mov|avi|webm').matchAsPrefix(element.path.split('.').last) != null).forEach((element) => sizelist.add(element.statSync().size));
+                          Directory(Hive.box('config').get('downloadPath') ?? '/storage/emulated/0/Download/').listSync().where((element) => RegExp('mp4|m4v|m4p|amv|mov|avi|webm|ogg').matchAsPrefix(element.path.split('.').last) != null).forEach((element) => sizelist.add(element.statSync().size));
                           num size = 0;
                           for (var el in sizelist) {
                             size += el;
@@ -153,7 +153,7 @@ class _DownloadsState extends State<Downloads> {
               child: Wrap(
                 spacing: 5,
                 runSpacing: 10,
-                children: snap.where((element) => RegExp('mp4|m4v|m4p|amv|mov|avi|webm').matchAsPrefix(element.path.split('.').last) != null).map((e) => DownloadedMovie(File(e.path))).toList(),
+                children: snap.where((element) => RegExp('mp4|m4v|m4p|amv|mov|avi|webm|ogg').matchAsPrefix(element.path.split('.').last) != null).map((e) => DownloadedMovie(File(e.path))).toList(),
               ),
             );
             // return Center(
