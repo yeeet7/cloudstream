1fa1280c0c32ba942a5c92e929ddab3b0ec86b3f
Viktor Kovalik <viktor.kovalik171@gmail.com>
2024-06-28 17:42:13 +0200

    show status bar and home indicator

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,7 @@ import 'package:flutter/services.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
+  await SystemChrome.setEnabledSystemUIMode(SystemUiMode.edgeToEdge);
   await Hive.initFlutter();
   await Hive.openBox('config');
   await Hive.openBox('downloadPosters');
