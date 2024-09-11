b8158995ed321a4aa4104433130c82f6b548bff2
Viktor Kovalik <viktor.kovalik171@gmail.com>
2024-09-11 19:01:40 +0200

    bookmarks fixed appbar

--- lib/view/primary/bookmark.dart
+++ lib/view/primary/bookmark.dart
@@ -39,116 +39,107 @@ class _BookmarkWidgetState extends State<BookmarkWidget> {
   Widget build(BuildContext context) {
     return Scaffold(
 
-      body: CustomScrollView(
-        controller: bookmarksScrollCtrl..addListener(() => BookmarksStateStorage.scrollOffset = bookmarksScrollCtrl.offset),
-        shrinkWrap: true,
-        slivers: [
-          SliverAppBar(
-            floating: true,
-            automaticallyImplyLeading: false,
-            surfaceTintColor: Colors.transparent,
-            title: TextField(
-              controller: bookmarksTextCtrl,
-              onSubmitted: (text) {},
-              decoration: InputDecoration(
-                hintText: 'Search...',
-                hintStyle: const TextStyle(color: Colors.white70),
-                prefixIcon: Container(margin: const EdgeInsets.all(10), child: PictureIcon('assets/search.png', size: 20,)),
-                prefixIconConstraints: const BoxConstraints(minHeight: 20, minWidth: 20),
-                fillColor: Colors.black,
-                filled: true,
-                isDense: true,
-                contentPadding: const EdgeInsets.symmetric(horizontal: 12, vertical: 12),
-                border: OutlineInputBorder(borderRadius: BorderRadius.circular(24), borderSide: const BorderSide(color: Colors.black)),
-                focusedBorder: OutlineInputBorder(borderRadius: BorderRadius.circular(24), borderSide: const BorderSide(color: Colors.black)),
-                enabledBorder: OutlineInputBorder(borderRadius: BorderRadius.circular(24), borderSide: const BorderSide(color: Colors.black)),
-              ),
-            ),
-            actions: [
-              IconButton(
-                icon: const Icon(FontAwesomeIcons.arrowDownWideShort),
-                onPressed: () async {
-                  await showModalBottomSheet(
-                    context: context,
-                    elevation: 0,
-                    backgroundColor: Colors.transparent,
-                    builder: (context) {
-                      return StatefulBuilder(
-                        builder: (context, setstate) {
-                          return Container(
-                            width: MediaQuery.of(context).size.width - 24,
-                            margin: const EdgeInsets.all(12),
-                            // alignment: Alignment.center,
-                            decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(24),
-                              color: Theme.of(context).bottomNavigationBarTheme.backgroundColor
-                            ),
-                            child: Wrap(
-                              alignment: WrapAlignment.center,
-                              children: [
-                                CustomChip('Custom', BookmarksStateStorage.sortType == SortType.custom, unselectedColor: const Color(0xFF212121), onTap: () async {await Hive.box('config').put('sortType', SortType.custom.index); setstate(() => BookmarksStateStorage.sortType = SortType.custom);}),
-                                CustomChip('Date Added', BookmarksStateStorage.sortType == SortType.dateAdded, unselectedColor: const Color(0xFF212121), onTap: () async {await Hive.box('config').put('sortType', SortType.dateAdded.index); setstate(() => BookmarksStateStorage.sortType = SortType.dateAdded);}),
-                                CustomChip('Name', BookmarksStateStorage.sortType == SortType.name, unselectedColor: const Color(0xFF212121), onTap: () async {await Hive.box('config').put('sortType', SortType.name.index); setstate(() => BookmarksStateStorage.sortType = SortType.name);}),
-                                CustomChip('Average Rating', BookmarksStateStorage.sortType == SortType.averageRating, unselectedColor: const Color(0xFF212121), onTap: () async {await Hive.box('config').put('sortType', SortType.averageRating.index); setstate(() => BookmarksStateStorage.sortType = SortType.averageRating);}),
-                                CustomChip('Release Year', BookmarksStateStorage.sortType == SortType.releaseYear, unselectedColor: const Color(0xFF212121), onTap: () async {await Hive.box('config').put('sortType', SortType.releaseYear.index); setstate(() => BookmarksStateStorage.sortType = SortType.releaseYear);}),
-                                CustomChip('User Rating', BookmarksStateStorage.sortType == SortType.userRating, unselectedColor: const Color(0xFF212121), onTap: () async {await Hive.box('config').put('sortType', SortType.userRating.index); setstate(() => BookmarksStateStorage.sortType = SortType.userRating);}),
-                                SizedBox(width: MediaQuery.of(context).size.width, height: 12,),
-                                CustomChip('Ascending', BookmarksStateStorage.sortDirIsAsc == true, unselectedColor: const Color(0xFF212121), onTap: () async {await Hive.box('config').put('sortDirIsAsc', true); setstate(() => BookmarksStateStorage.sortDirIsAsc = true);}),
-                                CustomChip('Descending', BookmarksStateStorage.sortDirIsAsc == false, unselectedColor: const Color(0xFF212121), onTap: () async {await Hive.box('config').put('sortDirIsAsc', false); setstate(() => BookmarksStateStorage.sortDirIsAsc = false);}),
-                              ],
-                            ),
-                          );
-                        }
+      appBar: AppBar(
+        // floating: true,
+        automaticallyImplyLeading: false,
+        surfaceTintColor: Colors.transparent,
+        title: TextField(
+          controller: bookmarksTextCtrl,
+          onSubmitted: (text) {},
+          decoration: InputDecoration(
+            hintText: 'Search...',
+            hintStyle: const TextStyle(color: Colors.white70),
+            prefixIcon: Container(margin: const EdgeInsets.all(10), child: PictureIcon('assets/search.png', size: 20,)),
+            prefixIconConstraints: const BoxConstraints(minHeight: 20, minWidth: 20),
+            fillColor: Colors.black,
+            filled: true,
+            isDense: true,
+            contentPadding: const EdgeInsets.symmetric(horizontal: 12, vertical: 12),
+            border: OutlineInputBorder(borderRadius: BorderRadius.circular(24), borderSide: const BorderSide(color: Colors.black)),
+            focusedBorder: OutlineInputBorder(borderRadius: BorderRadius.circular(24), borderSide: const BorderSide(color: Colors.black)),
+            enabledBorder: OutlineInputBorder(borderRadius: BorderRadius.circular(24), borderSide: const BorderSide(color: Colors.black)),
+          ),
+        ),
+        actions: [
+          IconButton(
+            icon: const Icon(FontAwesomeIcons.arrowDownWideShort),
+            onPressed: () async {
+              await showModalBottomSheet(
+                context: context,
+                elevation: 0,
+                backgroundColor: Colors.transparent,
+                builder: (context) {
+                  return StatefulBuilder(
+                    builder: (context, setstate) {
+                      return Container(
+                        width: MediaQuery.of(context).size.width - 24,
+                        margin: const EdgeInsets.all(12),
+                        // alignment: Alignment.center,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(24),
+                          color: Theme.of(context).bottomNavigationBarTheme.backgroundColor
+                        ),
+                        child: Wrap(
+                          alignment: WrapAlignment.center,
+                          children: [
+                            CustomChip('Custom', BookmarksStateStorage.sortType == SortType.custom, unselectedColor: const Color(0xFF212121), onTap: () async {await Hive.box('config').put('sortType', SortType.custom.index); setstate(() => BookmarksStateStorage.sortType = SortType.custom);}),
+                            CustomChip('Date Added', BookmarksStateStorage.sortType == SortType.dateAdded, unselectedColor: const Color(0xFF212121), onTap: () async {await Hive.box('config').put('sortType', SortType.dateAdded.index); setstate(() => BookmarksStateStorage.sortType = SortType.dateAdded);}),
+                            CustomChip('Name', BookmarksStateStorage.sortType == SortType.name, unselectedColor: const Color(0xFF212121), onTap: () async {await Hive.box('config').put('sortType', SortType.name.index); setstate(() => BookmarksStateStorage.sortType = SortType.name);}),
+                            CustomChip('Average Rating', BookmarksStateStorage.sortType == SortType.averageRating, unselectedColor: const Color(0xFF212121), onTap: () async {await Hive.box('config').put('sortType', SortType.averageRating.index); setstate(() => BookmarksStateStorage.sortType = SortType.averageRating);}),
+                            CustomChip('Release Year', BookmarksStateStorage.sortType == SortType.releaseYear, unselectedColor: const Color(0xFF212121), onTap: () async {await Hive.box('config').put('sortType', SortType.releaseYear.index); setstate(() => BookmarksStateStorage.sortType = SortType.releaseYear);}),
+                            CustomChip('User Rating', BookmarksStateStorage.sortType == SortType.userRating, unselectedColor: const Color(0xFF212121), onTap: () async {await Hive.box('config').put('sortType', SortType.userRating.index); setstate(() => BookmarksStateStorage.sortType = SortType.userRating);}),
+                            SizedBox(width: MediaQuery.of(context).size.width, height: 12,),
+                            CustomChip('Ascending', BookmarksStateStorage.sortDirIsAsc == true, unselectedColor: const Color(0xFF212121), onTap: () async {await Hive.box('config').put('sortDirIsAsc', true); setstate(() => BookmarksStateStorage.sortDirIsAsc = true);}),
+                            CustomChip('Descending', BookmarksStateStorage.sortDirIsAsc == false, unselectedColor: const Color(0xFF212121), onTap: () async {await Hive.box('config').put('sortDirIsAsc', false); setstate(() => BookmarksStateStorage.sortDirIsAsc = false);}),
+                          ],
+                        ),
                       );
                     }
-                  ).then((value) => setState(() {}));
-                },
-              ),
-            ],
-          ),
-
-          SliverList(
-            delegate: SliverChildListDelegate([
-              Container(
-                margin: const EdgeInsets.all(5),
-                child: BookmarksStateStorage.sortType != SortType.custom ? Wrap(
-                  spacing: 5,
-                  runSpacing: 10,
-                  children: [
-                    if(BookmarksStateStorage.type == BookmarkType.watching) ...snapshot.watching.sortByType(BookmarksStateStorage.sortType, BookmarksStateStorage.sortDirIsAsc).map<Widget>((e) => Movie(e.value)).toList(),
-                    if(BookmarksStateStorage.type == BookmarkType.completed) ...snapshot.completed.sortByType(BookmarksStateStorage.sortType, BookmarksStateStorage.sortDirIsAsc).map<Widget>((e) => Movie(e.value)).toList(),
-                    if(BookmarksStateStorage.type == BookmarkType.planned) ...snapshot.planned.sortByType(BookmarksStateStorage.sortType, BookmarksStateStorage.sortDirIsAsc).map<Widget>((e) => Movie(e.value)).toList(),
-                    if(BookmarksStateStorage.type == BookmarkType.onHold) ...snapshot.onHold.sortByType(BookmarksStateStorage.sortType, BookmarksStateStorage.sortDirIsAsc).map<Widget>((e) => Movie(e.value)).toList(),
-                    if(BookmarksStateStorage.type == BookmarkType.dropped) ...snapshot.dropped.sortByType(BookmarksStateStorage.sortType, BookmarksStateStorage.sortDirIsAsc).map<Widget>((e) => Movie(e.value)).toList(),
-                    ...List.generate(2, (index) => SizedBox(width: (MediaQuery.of(context).size.width - 20) / 3)),
-                  ],
-                ) : ReorderableWrap(
-                  spacing: 5,
-                  runSpacing: 10,
-                  children: [
-                    if(BookmarksStateStorage.type == BookmarkType.watching) ...snapshot.watching.sortByType(BookmarksStateStorage.sortType, BookmarksStateStorage.sortDirIsAsc).map<Widget>((e) => Movie(e.value, longTapShowsDetails: false)).toList(),
-                    if(BookmarksStateStorage.type == BookmarkType.completed) ...snapshot.completed.sortByType(BookmarksStateStorage.sortType, BookmarksStateStorage.sortDirIsAsc).map<Widget>((e) => Movie(e.value, longTapShowsDetails: false)).toList(),
-                    if(BookmarksStateStorage.type == BookmarkType.planned) ...snapshot.planned.sortByType(BookmarksStateStorage.sortType, BookmarksStateStorage.sortDirIsAsc).map<Widget>((e) => Movie(e.value, longTapShowsDetails: false)).toList(),
-                    if(BookmarksStateStorage.type == BookmarkType.onHold) ...snapshot.onHold.sortByType(BookmarksStateStorage.sortType, BookmarksStateStorage.sortDirIsAsc).map<Widget>((e) => Movie(e.value, longTapShowsDetails: false)).toList(),
-                    if(BookmarksStateStorage.type == BookmarkType.dropped) ...snapshot.dropped.sortByType(BookmarksStateStorage.sortType, BookmarksStateStorage.sortDirIsAsc).map<Widget>((e) => Movie(e.value, longTapShowsDetails: false)).toList(),
-                    ...List.generate(2, (index) => SizedBox(width: (MediaQuery.of(context).size.width - 20) / 3)),
-                  ],
-                  onReorder: (oI, nI) async {
-                    await Bookmarks.set(
-                      watching: BookmarksStateStorage.type == BookmarkType.watching ? (snapshot.watching..insert(nI, snapshot.watching.removeAt(oI))) : snapshot.watching,
-                      planned: BookmarksStateStorage.type == BookmarkType.planned ? (snapshot.planned..insert(nI, snapshot.planned.removeAt(oI))) : snapshot.planned,
-                      completed: BookmarksStateStorage.type == BookmarkType.completed ? (snapshot.completed..insert(nI, snapshot.completed.removeAt(oI))) : snapshot.completed,
-                      onHold: BookmarksStateStorage.type == BookmarkType.onHold ? (snapshot.onHold..insert(nI, snapshot.onHold.removeAt(oI))) : snapshot.onHold,
-                      dropped: BookmarksStateStorage.type == BookmarkType.dropped ? (snapshot.dropped..insert(nI, snapshot.dropped.removeAt(oI))) : snapshot.dropped,
-                    );
-                    setState(() {});
-                  }
-                ),
-              ),
-            ])
+                  );
+                }
+              ).then((value) => setState(() {}));
+            },
           ),
         ],
+      ),
 
+      body: SingleChildScrollView(
+        child: Container(
+          margin: const EdgeInsets.all(5),
+          child: BookmarksStateStorage.sortType != SortType.custom ? Wrap(
+            spacing: 5,
+            runSpacing: 10,
+            children: [
+              if(BookmarksStateStorage.type == BookmarkType.watching) ...snapshot.watching.sortByType(BookmarksStateStorage.sortType, BookmarksStateStorage.sortDirIsAsc).map<Widget>((e) => Movie(e.value)).toList(),
+              if(BookmarksStateStorage.type == BookmarkType.completed) ...snapshot.completed.sortByType(BookmarksStateStorage.sortType, BookmarksStateStorage.sortDirIsAsc).map<Widget>((e) => Movie(e.value)).toList(),
+              if(BookmarksStateStorage.type == BookmarkType.planned) ...snapshot.planned.sortByType(BookmarksStateStorage.sortType, BookmarksStateStorage.sortDirIsAsc).map<Widget>((e) => Movie(e.value)).toList(),
+              if(BookmarksStateStorage.type == BookmarkType.onHold) ...snapshot.onHold.sortByType(BookmarksStateStorage.sortType, BookmarksStateStorage.sortDirIsAsc).map<Widget>((e) => Movie(e.value)).toList(),
+              if(BookmarksStateStorage.type == BookmarkType.dropped) ...snapshot.dropped.sortByType(BookmarksStateStorage.sortType, BookmarksStateStorage.sortDirIsAsc).map<Widget>((e) => Movie(e.value)).toList(),
+              ...List.generate(2, (index) => SizedBox(width: (MediaQuery.of(context).size.width - 20) / 3)),
+            ],
+          ) : ReorderableWrap(
+            spacing: 5,
+            runSpacing: 10,
+            children: [
+              if(BookmarksStateStorage.type == BookmarkType.watching) ...snapshot.watching.sortByType(BookmarksStateStorage.sortType, BookmarksStateStorage.sortDirIsAsc).map<Widget>((e) => Movie(e.value, longTapShowsDetails: false)).toList(),
+              if(BookmarksStateStorage.type == BookmarkType.completed) ...snapshot.completed.sortByType(BookmarksStateStorage.sortType, BookmarksStateStorage.sortDirIsAsc).map<Widget>((e) => Movie(e.value, longTapShowsDetails: false)).toList(),
+              if(BookmarksStateStorage.type == BookmarkType.planned) ...snapshot.planned.sortByType(BookmarksStateStorage.sortType, BookmarksStateStorage.sortDirIsAsc).map<Widget>((e) => Movie(e.value, longTapShowsDetails: false)).toList(),
+              if(BookmarksStateStorage.type == BookmarkType.onHold) ...snapshot.onHold.sortByType(BookmarksStateStorage.sortType, BookmarksStateStorage.sortDirIsAsc).map<Widget>((e) => Movie(e.value, longTapShowsDetails: false)).toList(),
+              if(BookmarksStateStorage.type == BookmarkType.dropped) ...snapshot.dropped.sortByType(BookmarksStateStorage.sortType, BookmarksStateStorage.sortDirIsAsc).map<Widget>((e) => Movie(e.value, longTapShowsDetails: false)).toList(),
+              ...List.generate(2, (index) => SizedBox(width: (MediaQuery.of(context).size.width - 20) / 3)),
+            ],
+            onReorder: (oI, nI) async {
+              await Bookmarks.set(
+                watching: BookmarksStateStorage.type == BookmarkType.watching ? (snapshot.watching..insert(nI, snapshot.watching.removeAt(oI))) : snapshot.watching,
+                planned: BookmarksStateStorage.type == BookmarkType.planned ? (snapshot.planned..insert(nI, snapshot.planned.removeAt(oI))) : snapshot.planned,
+                completed: BookmarksStateStorage.type == BookmarkType.completed ? (snapshot.completed..insert(nI, snapshot.completed.removeAt(oI))) : snapshot.completed,
+                onHold: BookmarksStateStorage.type == BookmarkType.onHold ? (snapshot.onHold..insert(nI, snapshot.onHold.removeAt(oI))) : snapshot.onHold,
+                dropped: BookmarksStateStorage.type == BookmarkType.dropped ? (snapshot.dropped..insert(nI, snapshot.dropped.removeAt(oI))) : snapshot.dropped,
+              );
+              setState(() {});
+            }
+          ),
+        ),
       ),
 
       bottomNavigationBar: Container(
