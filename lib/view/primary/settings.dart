e425c8c0fd28fc6cf857344283960105414bdff8
Viktor Kovalik <viktor.kovalik171@gmail.com>
2024-10-04 16:50:57 +0200

    remove import 'dart:ui';

--- lib/view/primary/settings.dart
+++ lib/view/primary/settings.dart
@@ -1,11 +1,9 @@
 
 // ignore_for_file: use_build_context_synchronously
-
 import 'dart:async';
 import 'dart:convert';
 import 'dart:io';
 import 'dart:math' as math;
-import 'dart:ui';
 import 'package:cloudstream/main.dart';
 import 'package:cloudstream/widgets.dart';
 import 'package:file_picker/file_picker.dart';
